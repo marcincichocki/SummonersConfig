15ef9dc99e23d7b244102864bc71fb00b8b2b873
marcincichocki <marcincichocki1993@gmail.com>
2015-12-09 00:18:42 +0100

    Imports(rp & q) and first shape on request handler.

--- app.ts
+++ app.ts
@@ -1,7 +1,11 @@
 /// <reference path="typings/node/node.d.ts"/>
 /// <reference path="typings/express/express.d.ts"/>
+/// <reference path="typings/request-promise/request-promise.d.ts"/>
+/// <reference path="typings/q/Q.d.ts"/>
 
 import express = require('express');
+import rp = require('request-promise');
+import q = require('q');
 
 const PORT: number = 3000;
 const app: express.Express = express();
@@ -11,4 +15,14 @@ app.use(express.static('./'));
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
+});
+
+/**
+ * Request handler.
+ *
+ * Example:
+ * http://localhost:3000/na/hiimgosu
+ */
+app.get('/:server/:name', (req, res) => {
+  // Complicated logic here.
 });
