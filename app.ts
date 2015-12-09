b394f3c1682dde4f5e80503fce463b251f69b16f
marcincichocki <marcincichocki1993@gmail.com>
2015-12-09 01:42:26 +0100

    Request logic.

--- app.ts
+++ app.ts
@@ -7,6 +7,10 @@ import express = require('express');
 import rp = require('request-promise');
 import q = require('q');
 
+import {Summoner} from './server/Summoner';
+import {Url} from './server/Url';
+import API_KEY from './server/api';
+
 const PORT: number = 3000;
 const app: express.Express = express();
 
@@ -24,5 +28,46 @@ app.listen(PORT, () => {
  * http://localhost:3000/na/hiimgosu
  */
 app.get('/:server/:name', (req, res) => {
-  // Complicated logic here.
+
+  // Summoner server.
+  const server: string = req.params.server;
+
+  // Standardized summoner name.
+  const name: string = req.params.name.toLowerCase().replace(/ /g, '');
+
+  // Object which contains urls.
+  const url: Url = new Url(server, name);
+
+  console.log('Server: %s, name: %s', server, name);
+
+  rp({
+    uri: url.info(API_KEY),
+    json: true
+  }).then((data) => {
+
+    // Summoner information.
+    const summoner: Summoner = data[name];
+
+
+    q.all([
+      rp({
+        uri: url.asset('runes', summoner.id, API_KEY),
+        json: true
+      }),
+      rp({
+        uri: url.asset('masteries', summoner.id, API_KEY),
+        json: true
+      })
+    ]).spread((runes, masteries) => {
+      res.send({
+        runes,
+        masteries,
+        summoner
+      })
+    });
+
+  }).catch((error) => {
+    console.error('Error during lookup for summoner information\nerror: %d', error.statusCode);
+    res.status(error.statusCode);
+  });
 });
