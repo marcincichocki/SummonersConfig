af41e139a357ef43a7877e8a3704bba658565445
marcincichocki <marcincichocki1993@gmail.com>
2015-12-15 20:51:38 +0100

    Updated logic. Season start date is exported.

--- server/safeguard.ts
+++ server/safeguard.ts
@@ -4,16 +4,23 @@
 import moment = require('moment');
 
 // Date which determines when new season started.
-const SEASON_6: moment.Moment = moment('2015-12-1');
+export const SEASON_START: moment.Moment = moment('2015-12-1');
 
-/**
- * Check if account was visited after season update. If it was, we can
- * be sure that old masteries are removed, and recived data will not break
- * the application.
- * @param {number} revisionDate - Miliseconds representing date of last
- * activity on the checked account.
- * @return {boolean}
- */
-export default function isSafe(revisionDate: number): boolean {
-  return moment(revisionDate).isAfter(SEASON_6);
-}
+
+export const isSafe = (function() {
+
+  // Date which determines when new season started.
+  const seasonStart: moment.Moment = SEASON_START;
+
+  /**
+   * Check if account was visited after season update. If it was, we can
+   * be sure that old masteries are removed, and recived data will not break
+   * the application.
+   * @param {number} revisionDate - Miliseconds representing date of last
+   * activity on the checked account.
+   * @return {boolean}
+   */
+  return function(revisionDate: number): boolean {
+    return moment(revisionDate).isAfter(seasonStart);
+  }
+})();
