1cd8924acfbbf66ae0db87ab647378f0445f2458
marcincichocki <marcincichocki1993@gmail.com>
2015-12-15 22:53:40 +0100

    Fixme pls

--- server/safeguard.ts
+++ server/safeguard.ts
@@ -3,6 +3,9 @@
 
 import moment = require('moment');
 
+// FIXME: This should be date of last BREAKING change done to masteries.
+// For example: new mastery added, or max points of mastery lowered etc.
+//
 // Date which determines when new season started.
 export const SEASON_START: moment.Moment = moment('2015-11-11 11:01 +0000', 'YYYY-MM-DD HH:mm Z');
 
