411c67b1ee309efd129a329bea0c70e3273605d4
marcincichocki <marcincichocki1993@gmail.com>
2015-12-19 13:24:37 +0100

    Using percent pipe instead of service method.

--- src/app/components/tooltipRuneSums/tooltipRuneSums.component.ts
+++ src/app/components/tooltipRuneSums/tooltipRuneSums.component.ts
@@ -2,6 +2,7 @@ import {Component, Input} from 'angular2/core';
 import {NgFor} from 'angular2/common';
 
 import {RuneService} from '../../services/rune/rune.service';
+import {Percent} from '../../pipes/percent.pipe';
 
 
 @Component({
@@ -10,12 +11,13 @@ import {RuneService} from '../../services/rune/rune.service';
     <div class="tooltip-rune-sums">
       <h3 class="name">#{{data.index + 1}} - {{ runeService.getName(data.index) }}</h3>
       <p class="sum" *ngFor="#sum of data.sums">
-        <span class="value">{{ sum.value }}{{ runeService.isPercentage(sum.unitId) }}</span>
+        <span class="value">{{ sum.value }}{{ sum.unitId | percent }}</span>
         <span class="unit">{{ runeService.stats[sum.unitId] }}</span>
       </p>
     </div>
   `,
-  directives: [NgFor]
+  directives: [NgFor],
+  pipes: [Percent]
 })
 export class TooltipRuneSumsComponent {
   @Input() data;
